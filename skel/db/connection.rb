119261402830a0256144fd23be7fde529821868e
qjpcpu <qjpcpu@gmail.com>
2014-02-13 15:54:34 +0800

    redirect stdout to /dev/null when db is in memory

--- skel/db/connection.rb
+++ skel/db/connection.rb
@@ -4,5 +4,7 @@ dbconfig = YAML::load(File.open(File.expand_path('..',__FILE__)+'/database.yml')
 ActiveRecord::Base.establish_connection(dbconfig) 
 
 if dbconfig['database']==':memory:'
+  $stdout = File.new( '/dev/null', 'w' )
 	ActiveRecord::Migrator.migrate('db/migrate', ENV["VERSION"] ? ENV["VERSION"].to_i : nil )
+  $stdout = STDOUT
 end
