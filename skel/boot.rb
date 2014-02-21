b1968d20497801ec2940d7109b1c1820f2da3dd7
qujianping <qujianping@baidu.com>
2014-02-21 17:16:02 +0800

    fix bugs

--- skel/boot.rb
+++ skel/boot.rb
@@ -15,7 +15,7 @@ Dir[JreportRoot+'/config/*.rb'].each{|config| require config }
 Dir[JreportRoot+'/helpers/*_helper.rb'].each do |hlp|
   hlp_name=File.basename(hlp)[0..-4].split('_').map{|x| x.capitalize}.join
   require hlp
-  include Kernel.const_get(hlp_name) if Kernel.const_defined?(hlp_name)
+  include Kernel.const_get(hlp_name) 
 end
 # Load collectors and controllers
 Dir[JreportRoot+'/collectors/*.rb'].each{|c| require c }
